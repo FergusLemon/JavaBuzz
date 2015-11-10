cf59dd94b5208154a2d8ccd4ab258210c65ae2c6
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-10 10:31:14 +0000

    remove comments

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -5,60 +5,6 @@ describe('Javabuzz', function() {
     javabuzz = new Javabuzz();
   });
 
-  // describe('knows when a number is', function() {
-  //
-  //   it('divisible by 3', function() {
-  //     expect(javabuzz.isDivisibleByThree(3)).toBe(true);
-  //
-  //   });
-  //
-  // });
-  //
-  // describe('knows when a number is NOT', function() {
-  //
-  //   it('divisible by 3', function() {
-  //     expect(javabuzz.isDivisibleByThree(1)).toBe(false);
-  //
-  //   });
-  //
-  // });
-  //
-  // describe('knows when a number is', function() {
-  //
-  //   it('divisible by 5', function() {
-  //     expect(javabuzz.isDivisibleByFive(5)).toBe(true);
-  //
-  //   });
-  //
-  // });
-  //
-  // describe('knows when a number is NOT', function() {
-  //
-  //   it('divisible by 5', function() {
-  //     expect(javabuzz.isDivisibleByFive(1)).toBe(false);
-  //
-  //   });
-  //
-  // });
-  //
-  // describe('knows when a number is', function() {
-  //
-  //   it('divisible by 15', function() {
-  //     expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
-  //
-  //   });
-  //
-  // });
-  //
-  // describe('knows when a number is NOT', function() {
-  //
-  //   it('divisible by 15', function() {
-  //     expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
-  //
-  //   });
-  //
-  // });
-
   describe('when playing, says', function() {
 
     it('"Java" when a number is divisible by 3', function() {
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,17 +1,5 @@
 var Javabuzz = function() {};
 
-// Javabuzz.prototype.isDivisibleByThree = function(number) {
-//   return this._isDivisibleBy(number, 3);
-// };
-//
-// Javabuzz.prototype.isDivisibleByFive = function(number) {
-//   return this._isDivisibleBy(number, 5);
-// };
-//
-// Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-//   return this._isDivisibleBy(number, 15);
-// };
-
 Javabuzz.prototype.isDivisibleBy = function(number, divisor) {
   return (number % divisor === 0);
 };
