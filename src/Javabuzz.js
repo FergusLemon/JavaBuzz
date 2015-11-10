cd36a1f9e355dedae882ff9821c8129941e485d5
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-10 10:29:17 +0000

    refactor to remove superfluous tests

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,18 +1,18 @@
 var Javabuzz = function() {};
 
-Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return this._isDivisibleBy(number, 3);
-};
+// Javabuzz.prototype.isDivisibleByThree = function(number) {
+//   return this._isDivisibleBy(number, 3);
+// };
+//
+// Javabuzz.prototype.isDivisibleByFive = function(number) {
+//   return this._isDivisibleBy(number, 5);
+// };
+//
+// Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+//   return this._isDivisibleBy(number, 15);
+// };
 
-Javabuzz.prototype.isDivisibleByFive = function(number) {
-  return this._isDivisibleBy(number, 5);
-};
-
-Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-  return this._isDivisibleBy(number, 15);
-};
-
-Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+Javabuzz.prototype.isDivisibleBy = function(number, divisor) {
   return (number % divisor === 0);
 };
 
@@ -20,13 +20,13 @@ Javabuzz.prototype.says = function(number) {
   if (number === 0) {
     return number;
   }
-  if (this.isDivisibleByFifteen(number)) {
+  if (this.isDivisibleBy(number, 15)) {
     return "JavaBuzz";
   }
-  if (this.isDivisibleByFive(number)) {
+  if (this.isDivisibleBy(number, 5)) {
     return "Buzz";
   }
-  if (this.isDivisibleByThree(number)) {
+  if (this.isDivisibleBy(number, 3)) {
     return "Java";
   }
   return number;
